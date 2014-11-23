88ee32cf1198a7cabeee6b1a0fe2f0a6808611f2
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-11-23 19:48:45 +0900

    ignored no options for logger

--- lib/agig/client.rb
+++ lib/agig/client.rb
@@ -5,8 +5,10 @@ module Agig::Client
   def self.run
     opts = Agig::OptParser.parse!(ARGV)
 
-    opts[:logger] = Logger.new(opts[:log], "daily")
-    opts[:logger].level = opts[:debug] ? Logger::DEBUG : Logger::INFO
+    if opts[:log]
+      opts[:logger] = Logger.new(opts[:log], "daily")
+      opts[:logger].level = opts[:debug] ? Logger::DEBUG : Logger::INFO
+    end
 
     Net::IRC::Server.new(opts[:host], opts[:port], Agig::Session, opts).start
   end
